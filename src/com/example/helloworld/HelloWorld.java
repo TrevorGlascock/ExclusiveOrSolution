ae921be994b031732d238702fff645466837e86e
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-09-24 23:34:57 -0500

    ExclusiveOrFinal is made as a copy of exclusiveOr3

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
         List<Integer> listA = new ArrayList<Integer>(Arrays.asList(1, 3, 3, 6, 6, 7, 9));
         List<Integer> listB = new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7));
 
-        List<Integer> result = exclusiveOr3(listA,listB);
+        List<Integer> result = exclusiveOrFinal(listA,listB);
         System.out.println(result); // => [1,2,5,6,6,9]
     }
 
@@ -102,6 +102,49 @@ public class HelloWorld {
             }
         }
 
+
+        return result;
+    }
+
+    public static List<Integer> exclusiveOrFinal(List<Integer> listA, List<Integer> listB) {
+        List<Integer> result = new ArrayList<>();
+
+        Iterator<Integer> itA = listA.iterator();
+        Iterator<Integer> itB = listB.iterator();
+
+        int valA = itA.next();
+        int valB = itB.next();
+
+        while(itA.hasNext() || itB.hasNext()){
+            // valA is smaller AND itA has not terminated
+            if(valA<valB && itA.hasNext()) {
+                result.add(valA);
+                valA = itA.next();
+            }
+
+            // valB is smaller, AND itB has not terminated
+            else if(valB<valA && itB.hasNext()){
+                result.add(valB);
+                valB = itB.next();
+            }
+
+            // valA and valB are equal, AND they both have not terminated
+            else if(valA==valB && itA.hasNext() && itB.hasNext()){
+                int temp = valA;
+                while(valA==temp)
+                    valA=itA.next();
+
+                temp = valB;
+                while(valB==temp)
+                    valB=itB.next();
+            }
+
+            // itA or itB has reached the end
+            else{
+                while(itA.hasNext()) result.add(itA.next());
+                while(itB.hasNext()) result.add(itB.next());
+            }
+        }
 
         return result;
     }
